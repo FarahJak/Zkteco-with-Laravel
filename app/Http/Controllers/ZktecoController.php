42d3fba7757bc3854a5acab7789d0e0b9e537c4f
Farah_Jakmery <114393666+FarahJak@users.noreply.github.com>
2023-08-24 13:21:52 +0300

    Add and Implement index function

--- app/Http/Controllers/ZktecoController.php
+++ app/Http/Controllers/ZktecoController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ZktecoController extends Controller
 {
-    //
+    /**
+     *
+     * Get a list of the available attendance in the device.
+     *
+     */
+    public function index()
+    {
+        $zk = new ZKTeco('192.168.1.19', 4370);
+
+        if ($zk->connect()) {
+            $attendance = $zk->getAttendance();
+            return  $attendance;
+        }
+    }
 }
